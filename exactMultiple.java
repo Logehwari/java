b762bc6773166af2027a8f1925800d616195e917
Logeshwari <47217787+Logehwari@users.noreply.github.com>
2019-08-12 19:16:15 +0530

    Update exactMultiple.java

--- exactMultiple.java
+++ exactMultiple.java
@@ -1,1 +1,15 @@
+import java.io.*;
+import java.util.*;
 
+class UserManiCode
+{
+   public int exactMultiple(int input1, int input2){
+
+
+if(input1 % input2 == 0)
+return 2;
+else
+return 1;
+
+}
+}
