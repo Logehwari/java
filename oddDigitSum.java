bf7dc42a79aa352fcdc857a481eae4c7cdc48e46
Logeshwari <47217787+Logehwari@users.noreply.github.com>
2019-08-12 19:47:11 +0530

    Create oddDigitSum.java

---
+++ oddDigitSum.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class UserManiCode
+{
+   public int oddDigitSum(int input1){
+int st1=0, esum=0, osum=0;
+		while(input1!=0)
+		{
+			st1 = input1 % 10;
+			if(st1%2 == 0)
+			{
+				esum += st1;
+			}
+			else
+			{
+				osum +=st1;
+			}
+			input1 /= 10;
+		}
+	return osum;	
+	}
+}
