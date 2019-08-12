ffe79ecc73044c89adfbced775c6588cd7c91c35
Logeshwari <47217787+Logehwari@users.noreply.github.com>
2019-08-12 19:43:53 +0530

    Create oddOrEveDigitSum.java

---
+++ oddOrEveDigitSum.java
@@ -0,0 +1,26 @@
+int st1=0, esum=0, osum=0;
+		while(input1!=0)
+		{
+			st1 = input1 % 10;
+			if(st1%2 == 0)
+			{
+				esum += st1;
+			}
+			else
+			{
+				osum +=st1;
+			}
+			input1 /= 10;
+		}
+		if(input2.equals("even"))
+		{	
+			return esum;
+		}
+		else
+		{
+			return osum;
+		}	
+	
+		
+	}
+}
