6f047f6c23bad61ff6bc14957e5eb6b8d56e35c1
Logeshwari <47217787+Logehwari@users.noreply.github.com>
2019-08-12 12:24:29 +0530

    Update scndLastDigit.java

--- scndLastDigit.java
+++ scndLastDigit.java
@@ -1,1 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class UserManiCode
+{
+   public int secondLastDigit(int input1){
+
+int ls1 = Math.abs(input1);
+int ls2 = ls1 / 10;
+int ls3 = ls2 % 10;
+return ls3;
+
+}
+}
 
