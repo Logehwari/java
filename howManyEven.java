b8a9f502747cd124b7f12d0fbdb4437de0c30e71
Logeshwari <47217787+Logehwari@users.noreply.github.com>
2019-08-16 00:01:52 -0700

    Update howManyEven.java

--- howManyEven.java
+++ howManyEven.java
@@ -1,1 +1,21 @@
+ int s1, count=0,count1=0;
+		int[] arr = {input1, input2, input3, input4, input5};
+		for(int i=0; i<5; i++)
+		{
+			s1 = arr[i]%2;
+			if(s1==0)
+			{
+				count++;
+			}
+			else
+			{
+				count1++;
+			}
+		}
+		
+	
+    return count;
 
+		
+	}
+}
