92fe10ee7913a43a26275107208a71edf74bd0bd
Logeshwari <47217787+Logehwari@users.noreply.github.com>
2019-08-16 01:56:11 -0700

    Update sumOfLastDigits.java

--- sumOfLastDigits.java
+++ sumOfLastDigits.java
@@ -1,1 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class UserManiCode
+{
+   public int lastDigit(int input1, int input2){
+
+int ls1 = Math.abs(input1);
+int ls2 = Math.abs(input2); 
+int lss1 = ls1%10;
+int lss2 = ls2%10;
+int lg1 = lss1 + lss2;
+return lg1;
+
+}
+}
 
