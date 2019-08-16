d4ac13b0efdc5994b49c31a30cef915bc2625264
Logeshwari <47217787+Logehwari@users.noreply.github.com>
2019-08-16 01:47:17 -0700

    Update howManyEvenOrOdd.java

--- howManyEvenOrOdd.java
+++ howManyEvenOrOdd.java
@@ -4,3 +4,33 @@ import java.util.*;
 class UserManiCode
 {
    public int howManyEvenOrOdd(int input1, int input2){
+
+		int s1, count=0,count1=0;
+		int[] arr = {input1, input2, input3, input4, input5};
+		for(int i=0; i<5; i++)
+		{
+			s1 = arr[i]%2;
+			if(s1==0)
+			{
+				count++;
+			}
+			else
+			{
+				count1++;
+			}
+		}
+		
+	if(input6.equals("even"))
+    {
+		return count;
+
+	}
+	else
+	{
+		return count1;
+	}	
+	
+	
+	}
+	
+}
