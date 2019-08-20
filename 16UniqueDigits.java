d047be181921a431a09271f383260302939c8ded
Logeshwari <47217787+Logehwari@users.noreply.github.com>
2019-08-20 12:33:36 +0530

    Update 16UniqueDigits.java

--- 16UniqueDigits.java
+++ 16UniqueDigits.java
@@ -1,1 +1,22 @@
 
+int a[] = new int[10];
+		for(int i=0;i<10;i++)
+		{
+			a[i]=0;
+		}
+		int count=0;
+		while(input1 !=0)
+		{
+			int r=input1%10;
+			a[r]++;
+			if(a[r]==1)
+			{
+				count++;
+			}
+			input1 = input1/10;
+
+		}
+	return count;
+        		
+	}
+}
