7a178aa28f569cdd67ded054ccfa85166e494909
Logeshwari <47217787+Logehwari@users.noreply.github.com>
2019-08-21 20:48:55 -0700

    Update 14PrimeRange.java

--- 14PrimeRange.java
+++ 14PrimeRange.java
@@ -4,4 +4,30 @@ import java.util.*;
 class UserManiCode
 {
    public int 14PrimeRange(int input1, int input2){
+      int i =0;
+		int num =0;
+		int counter=0;
+		for (i = input1; i <= input2; i++)         
+		{ 		  	  
+			 	  
+		    for(num =i; num>=input1/2; num++)
+			{
+		        
+		    	if(i%num==0)
+				{
+		 			counter = counter + 1;
+				}
+			}
+			  
+		}   	
+		       
+		
+		
+		return counter;
+	
+
+
+		
+	}
+}
       
